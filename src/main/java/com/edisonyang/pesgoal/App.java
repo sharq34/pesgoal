e7293ebc79a0ed1939271b8a2a56458ef65d7be6
Edison Yang <sharq34@gmail.com>
2013-03-15 15:18:04 +0800

    beginning

---
+++ src/main/java/com/edisonyang/pesgoal/App.java
@@ -0,0 +1,13 @@
+package com.edisonyang.pesgoal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
